abcda72889dbf30aac78029ea3d48a07a885f588
freddieRv <federico.ram.v@gmail.com>
2018-04-19 23:04:06 -0500

    added more draft methods to model base class

--- models/model.js
+++ models/model.js
@@ -1,5 +1,21 @@
 class Model
 {
+
+    // table()
+    // {
+    //     return '';   -------------- IDEA: One way
+    // }
+
+    constructor()
+    {
+        // instantiate object with data from db
+    }
+
+    update(new_data)
+    {
+        // Iterate over new_data array and construct update query
+    }
+
     static all()
     {
         // Get all elements
@@ -15,5 +31,7 @@ class Model
         // Delete one or more elements
     }
 }
+
+// Model.table = ''; ----------- IDEA: Another way
 
 module.exports = Model;
