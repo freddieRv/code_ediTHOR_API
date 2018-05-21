e5e9a6721c5662eca6e942e5c82f99617b9aff01
freddieRv <federico.ram.v@gmail.com>
2018-05-21 12:35:25 -0500

    handle cors pre-flight options request

--- app.js
+++ app.js
@@ -2,21 +2,6 @@ var express = require('express');
 var app     = express();
 var router  = require('./routes');
 
-// Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', null);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.use('/', router);
 
 app.listen(3000, function () {
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,28 @@ const projects_router = require('./projects');
 const auth_router     = require('./auth');
 const router          = Router();
 
-// Middleware
+// Common headers, also handle CORS pre-flight OPTIONS request
+router.use(function (req, res, next) {
+    // Websites allowed to connect to the api
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
+    // Allowed request methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Allowed request headers
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
+
+    // Pass to next layer of middleware if neccesary
+    if (req.method == 'OPTIONS') {
+        res.send('Code EdiTHOR API v1.0');
+        next('router');
+    } else {
+        next();
+    }
+
+});
+
+// Middleware
 router.use(body_parser.urlencoded({
     extended: false
 }));
@@ -15,7 +35,6 @@ router.use(body_parser.urlencoded({
 router.use(body_parser.json());
 
 // Routes
-
 router.get('/', function(request, response) {
     response.send('Code EdiTHOR API v1.0');
 });
