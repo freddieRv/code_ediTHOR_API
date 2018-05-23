868881e698661471be87aa65c43d30d6216f4e59
freddieRv <federico.ram.v@gmail.com>
2018-05-23 14:04:08 -0500

    refactored projects middleware

--- middleware/projects.js
+++ middleware/projects.js
@@ -1,6 +1,32 @@
 const User    = require('../models/user');
 const Project = require('../models/project');
 
+function get_project_users(project_id, success, fail)
+{
+    Project.find(project_id)
+    .then(function(projects) {
+
+        if (projects == false) {
+            fail('Project not found');
+            return;
+        }
+
+        var project = new Project(projects[0].data);
+
+        project.users(User)
+        .then(function(users) {
+            success(users);
+        })
+        .catch(function(users_err) {
+            fail(users_err);
+        });
+
+    })
+    .catch(function(projects_err) {
+        fail(projects_err);
+    });
+}
+
 module.exports = {
     create_request(request, response, next)
     {
@@ -43,49 +69,54 @@ module.exports = {
 
     can_update(request, response, next)
     {
-        Project.find(request.params.id)
-        .then(function(projects) {
+        get_project_users(request.params.id, function(users) {
+            var allowed = false;
 
-            if (projects == false) {
-                response.status(404).send('Project not found');
-                next('router');
-                return;
-            }
-
-            var project = new Project(projects[0].data);
-
-            project.users(User)
-            .then(function(users) {
-                var allowed = false;
-
-                users.forEach(function(user) {
-                    if (user.id == request.authenticated_user_id) {
-                        allowed = true;
-                    }
-                });
-
-                if (allowed) {
-                    next();
-                } else {
-                    response.status(301).send("You dont have permission to update this project");
-                    next('router');
+            users.forEach(function(user) {
+                if (user.id == request.authenticated_user_id) {
+                    allowed = true;
                 }
-
-            })
-            .catch(function(users_err) {
-                response.status(500).send(users_err);
-                next('router');
             });
 
-        })
-        .catch(function(projects_err) {
-            response.status(500).send(projects_err);
+            if (allowed) {
+                next();
+            } else {
+                response.status(301).send("You dont have permission to update this project");
+                next('router');
+            }
+
+        }, function(err) {
+            response.status(500).send(err);
             next('router');
         });
     },
 
-    can_delete()
+    can_delete(request, response, next)
     {
+        get_project_users(request.params.id, function(users) {
+            var allowed = false;
+            var message = "You dont have permission to update this project";
 
+            users.forEach(function(user) {
+                if (user.id == request.authenticated_user_id) {
+                    if (user.role == 'project_admin') {
+                        allowed = true;
+                    } else {
+                        "You dont have permission to delete this project"
+                    }
+                }
+            });
+
+            if (allowed) {
+                next();
+            } else {
+                response.status(301).send(message);
+                next('router');
+            }
+
+        }, function(err) {
+            response.status(500).send(err);
+            next('router');
+        });
     }
 }
